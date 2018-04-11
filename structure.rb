0eea33042142b65b5c2103e2538e5c379aed5fb8
Michael Adams <mtadams007@gmail.com>
2018-04-11 10:53:08 -0400

    simple calculate method done

--- structure.rb
+++ structure.rb
@@ -1,5 +1,5 @@
 class Line
-  attr_reader :stops_arr :name :delays
+  attr_reader :stops_arr, :name, :delays
   def initialize (stops_arr,name)
     @stops_arr = stops_arr
     @name = name
@@ -7,7 +7,7 @@ class Line
   end
 
   def stops
-    puts @stops_arr
+    # puts @stops_arr
     @stops_arr
   end
 end
@@ -15,4 +15,14 @@ end
 def calculate(line1,start,line2,stop)
   if line1 == line2
     stops_arr = line1.stops
-    
+    num_of_stops = stops_arr.index(stop) - stops_arr.index(start)
+    p  num_of_stops.abs
+  end
+end
+
+N = Line.new(['Times Square', '34th', '28th', '23rd', 'Union Square', '8th'], "N")
+
+# p N.stops
+# p N
+calculate(N,'34th', N, "23rd")
+calculate(N,'23rd', N, "Times Square")
