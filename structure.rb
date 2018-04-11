d26d7808a83e227c5ee729e73ffc890572e7addd
Michael Adams <mtadams007@gmail.com>
2018-04-11 11:03:30 -0400

    calculate finished for non bonus

--- structure.rb
+++ structure.rb
@@ -12,17 +12,24 @@ class Line
   end
 end
 
+def mini_calc(start, stop, line)
+  stops_arr = line.stops
+  num_of_stops = stops_arr.index(stop) - stops_arr.index(start)
+  num_of_stops.abs
+end
+
 def calculate(line1,start,line2,stop)
   if line1 == line2
-    stops_arr = line1.stops
-    num_of_stops = stops_arr.index(stop) - stops_arr.index(start)
-    p  num_of_stops.abs
+    return mini_calc(start,stop,line1)
+  else
+    return mini_calc(start, "Union Square", line1) + mini_calc("Union Square", stop, line2)
   end
 end
 
 N = Line.new(['Times Square', '34th', '28th', '23rd', 'Union Square', '8th'], "N")
 
+L = Line.new(["8th", "6th", "Union Square", "3rd", "1st"], "L")
 # p N.stops
 # p N
-calculate(N,'34th', N, "23rd")
-calculate(N,'23rd', N, "Times Square")
+puts calculate(N,'23rd', N, "Times Square")
+puts calculate(L, '1st', N, "34th")
